2eef54fd18e611c869b72becda78b23c67124e66
Zac Johnson <zac@lightningkite.com>
2021-02-04 08:59:55 -0700

    Initial Commit

---
+++ TimeClock/TimeClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeClockApp.swift
+//  TimeClock
+//
+//  Created by Zac Johnson on 2/4/21.
+//
+
+import SwiftUI
+
+@main
+struct TimeClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
